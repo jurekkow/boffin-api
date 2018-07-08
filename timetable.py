d4e3d503611d239bc2a1140ec0b703750b0d1f31
Jerzy Kowalski <kowalskijerzy@aol.com>
2018-07-08 15:09:27 +0200

    Save timetable, use proper format and make safe when clashfinder fails

--- timetable.py
+++ timetable.py
@@ -1,8 +1,12 @@
 import collections
 import datetime
 import json
+import re
 
 import requests
+import requests.exceptions
+
+import paths
 
 TIMETABLE_URL = "https://clashfinder.com/data/event/offfestival2018.json"
 WEEK_DAYS = {
@@ -16,10 +20,20 @@ FEST_DAYS = [
     ("sunday", "05.08"),
 ]
 DAY_END_HOUR = 5
+HTTP_OK = 200
+
+
+with open(paths.TIMETABLE_FILE_PATH) as timetable_file:
+    boffin_timetable = json.load(timetable_file)
 
 
 def get_clashfinder_timetable():
-    response = requests.get(TIMETABLE_URL, verify=False)
+    try:
+        response = requests.get(TIMETABLE_URL, verify=False)
+    except requests.exceptions.ConnectionError:
+        return None
+    if response.status_code != HTTP_OK:
+        return None
     clashfinder_timetable = response.json()
     return clashfinder_timetable
 
@@ -34,6 +48,25 @@ def get_week_day(date_time):
     return week_day
 
 
+def get_artist_name(event_name):
+    event_name = event_name.strip().lower()
+    artist_plays_album_pattern = re.compile(
+        r"(?P<artist>.*) (gra|grają) (?P<album>.*)"
+    )
+    artist_plays_live_pattern = re.compile(
+        r"(?P<artist>.*) live"
+    )
+    artist_plays_album = re.match(artist_plays_album_pattern, event_name)
+    artist_plays_live = re.match(artist_plays_live_pattern, event_name)
+    if artist_plays_album:
+        artist_name = artist_plays_album.group("artist")
+    elif artist_plays_live:
+        artist_name = artist_plays_live.group("artist")
+    else:
+        artist_name = event_name
+    return artist_name
+
+
 def create_blank_boffin_timetable():
     boffin_timetable = {
         week_day: {"date": date, "stages": collections.defaultdict(list)}
@@ -45,19 +78,29 @@ def create_blank_boffin_timetable():
 def add_event_data(boffin_timetable, clashfinder_timetable):
     for location in clashfinder_timetable["locations"]:
         stage = location["name"].strip()
-        for event in location["events"]:
+        events = location["events"]
+        for event in events:
             week_day = get_week_day(event["start"])
             event_data = {
-                "name": event["name"].strip().lower(),
-                "from": event["start"].split(" ")[1],
-                "to": event["end"].split(" ")[1]
+                "title": event["name"].strip(),
+                "name": get_artist_name(event["name"]),
+                "start": event[ "start"],
+                "end": event["end"]
             }
             boffin_timetable[week_day]["stages"][stage].append(event_data)
+
     return boffin_timetable
 
 
 def get_current_timetable():
+    global boffin_timetable
+
     clashfinder_timetable = get_clashfinder_timetable()
+    if not clashfinder_timetable:
+        return boffin_timetable
+            
     boffin_timetable = create_blank_boffin_timetable()
     boffin_timetable = add_event_data(boffin_timetable, clashfinder_timetable)
+    with open(paths.TIMETABLE_FILE_PATH, "w") as timetable_file:
+        json.dump(boffin_timetable, timetable_file)
     return boffin_timetable
